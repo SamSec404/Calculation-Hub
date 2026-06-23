124a634e91773488109e26ba5887d04c4bf73df1
Sam <zerodaybypass48@gmail.com>
2026-06-24 00:38:47 +0500

    Update menu.py

---
+++ menu.py
@@ -0,0 +1,20 @@
+# utils/menu.py
+
+def show_main_menu():
+
+    print("\n==============================")
+    print("      CALCULATION HUB")
+    print("==============================")
+
+    print("1. Basic Calculator")
+    print("2. Scientific Calculator")
+    print("3. GPA Calculator")
+    print("4. CGPA Calculator")
+    print("5. Percentage Calculator")
+    print("6. BMI Calculator")
+    print("7. Age Calculator")
+    print("8. Area Calculator")
+    print("9. Perimeter Calculator")
+    print("10. Unit Converter")
+    print("11. History")
+    print("12. Exit")
