d0a87b0ba3e20640b44b61f2a8fc0bddd27c4c46
Sam <zerodaybypass48@gmail.com>
2026-06-24 00:40:50 +0500

    Update history.py

--- history.py
+++ history.py
@@ -25,3 +25,29 @@ def clear_history():
     history.clear()
 
     print("\nHistory cleared successfully.")
+
+from utils.file_handler import save_to_file
+from utils.file_handler import read_file
+
+HISTORY_FILE = "data/history.txt"
+
+
+def add_history(record):
+
+    save_to_file(HISTORY_FILE, record)
+
+
+def show_history():
+
+    records = read_file(HISTORY_FILE)
+
+    if not records:
+
+        print("\nNo history found.")
+        return
+
+    print("\n===== HISTORY =====")
+
+    for record in records:
+
+        print(record.strip())
