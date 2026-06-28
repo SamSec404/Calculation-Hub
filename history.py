74b0a5104a1a36de465ad887b3da27735fa36197
Sam <zerodaybypass48@gmail.com>
2026-06-28 15:18:42 +0500

    Update history.py

--- history.py
+++ history.py
@@ -1,31 +1,3 @@
-# utils/history.py
-
-history = []
-
-
-def add_history(record):
-    history.append(record)
-
-
-def show_history():
-
-    if len(history) == 0:
-
-        print("\nNo calculations found.")
-        return
-
-    print("\n===== HISTORY =====")
-
-    for index, item in enumerate(history, start=1):
-        print(f"{index}. {item}")
-
-
-def clear_history():
-
-    history.clear()
-
-    print("\nHistory cleared successfully.")
-
 from utils.file_handler import save_to_file
 from utils.file_handler import read_file
 
@@ -33,7 +5,6 @@ HISTORY_FILE = "data/history.txt"
 
 
 def add_history(record):
-
     save_to_file(HISTORY_FILE, record)
 
 
@@ -42,12 +13,18 @@ def show_history():
     records = read_file(HISTORY_FILE)
 
     if not records:
-
         print("\nNo history found.")
         return
 
     print("\n===== HISTORY =====")
 
-    for record in records:
+    for index, record in enumerate(records, start=1):
+        print(f"{index}. {record.strip()}")
 
-        print(record.strip())
+
+def clear_history():
+
+    with open(HISTORY_FILE, "w") as file:
+        pass
+
+    print("\nHistory cleared successfully.")
