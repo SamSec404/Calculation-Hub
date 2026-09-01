fa75dbfb76a9c81df0f7ddf51388e799a9232011
Sam <zerodaybypass48@gmail.com>
2026-09-01 23:37:59 +0500

    Added Tests

--- test/test_basic_calc.py
+++ test/test_basic_calc.py
@@ -7,30 +7,24 @@ from Calculators.basic_calc import power
 
 
 def test_add():
-
     assert add([10, 20, 30]) == 60
 
-
 def test_subtract():
-
     assert subtract([100, 20, 10]) == 70
 
-
 def test_multiply():
-
     assert multiply([2, 3, 4]) == 24
 
-
 def test_divide():
-
     assert divide([100, 2, 5]) == 10
 
-
 def test_modulus():
-
     assert modulus(10, 3) == 1
 
-
 def test_power():
-
     assert power(2, 3) == 8
+
+
+
+
+
