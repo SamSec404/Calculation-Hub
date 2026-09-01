cb584877948ffe12abdedea2b71cbe7380d2fd48
Sam <zerodaybypass48@gmail.com>
2026-09-01 23:36:40 +0500

    Added Tests

--- test/test_age.py
+++ test/test_age.py
@@ -1,12 +1,11 @@
 from calculators.age import age_in_months
 from calculators.age import age_in_days
 
-
 def test_age_months():
-
     assert age_in_months(20) == 240
 
-
 def test_age_days():
-
     assert age_in_days(1) == 365
+
+
+
