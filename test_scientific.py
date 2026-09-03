3e4ee846262d71162a84b19cd0277c3b321cd391
Sam <zerodaybypass48@gmail.com>
2026-09-03 20:37:40 +0500

    Added Tests

--- test_scientific.py
+++ test_scientific.py
@@ -3,22 +3,18 @@ from calculators.scientific import factorial
 from calculators.scientific import logarithm
 from calculators.scientific import power
 
-
 def test_square_root():
-
     assert square_root(25) == 5
 
-
 def test_factorial():
-
     assert factorial(5) == 120
 
-
 def test_logarithm():
-
     assert logarithm(100) == 2
 
-
 def test_power():
-
     assert power(2, 3) == 8
+
+
+
+
