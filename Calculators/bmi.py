f378ae1acf0a9ca2af78fb072c1acb6d9f8d24bb
Sam <zerodaybypass48@gmail.com>
2026-09-09 19:49:33 +0500

    Added calculators to Calculation-Hub

--- Calculators/bmi.py
+++ Calculators/bmi.py
@@ -2,20 +2,21 @@ def calculate_bmi(weight, height):
     if height <= 0:
         return "Error: Height must be greater than zero"
 
-    bmi = weight / (height ** 2)
+    bmi=weight/(height**2)
 
     return round(bmi, 2)
 
 def bmi_category(bmi):
-    if bmi < 18.5:
+    if bmi<18.5:
         return "Underweight"
 
-    elif bmi < 25:
+    elif bmi<25:
         return "Normal Weight"
 
-    elif bmi < 30:
+    elif bmi<30:
         return "Overweight"
 
     return "Obese"
+
 
 
